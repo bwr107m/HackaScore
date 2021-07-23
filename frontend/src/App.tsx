35d570d6c2bca5fc7e4ab1af0c0f2f4c06f55002
AustonLee <auston.cc.lee@mail.foxconn.com>
2021-07-23 09:05:59 +0800

    1. Add InputText. 2. Add Button. 3. Change logo.svg. 4. Cancel logo animation.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { InputText} from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 function App() {
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <InputText value={text} onChange={e => setText(e.target.value)} />
+        {text}
+        <Button type="button" label="Sumit" icon="pi pi-check"></Button>
       </header>
     </div>
   );
