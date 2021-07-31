eba331ed224ec50208c56bfe258df86b6d0938dd
AustonLee <auston.cc.lee@mail.foxconn.com>
2021-07-31 09:45:57 +0800

    1. Create a login page as main page(localhost:3000). 2. After logging in is OK, it jumps to /DataTableSelection. 3. Account and password are temporarily hardcoded(account=tsmc, password=tsmc) in backend server.ts for test.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,18 @@
-import './index.css';
-import DataTableSelection from './component/DataTableSelection';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import './index.css';
+import DataTableSelection from './component/DataTableSelection';
+import Login from './component/Login';
 
 ReactDOM.render(
-    <React.StrictMode>        
-        <DataTableSelection />
+    <React.StrictMode>
+      <BrowserRouter>        
+          <Route exact path="/" component={Login} />
+          <Route exact path="/DataTableSelection" component={DataTableSelection} />
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
